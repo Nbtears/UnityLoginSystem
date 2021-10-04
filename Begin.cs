621f8956db24e1a8b845bf57469d215a605aa60c
Diana Aguilar <dianaleticia2001@hotmail.com>
2021-10-04 10:27:45 -0500

    Pantalla de inicio conectada con el resto de canvas

--- Begin.cs
+++ Begin.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Begin : MonoBehaviour
 {
+    public Button logbutton;
+    public Button sign;
+    public GameObject scenelog;
+    public GameObject sceneReg;
+    public GameObject actualscene;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        logbutton.onClick.AddListener(loginbutton);
+        sign.onClick.AddListener(signbutton);
     }
 
-    // Update is called once per frame
-    void Update()
+    void loginbutton()
     {
-        
+        scenelog.SetActive(true);
+        actualscene.SetActive(false);
+    }
+
+    void signbutton()
+    {
+        sceneReg.SetActive(true);
+        actualscene.SetActive(false);
     }
 }
+
